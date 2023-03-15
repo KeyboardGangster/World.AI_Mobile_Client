49f4d3c586d182fbb48829b2dc4f1e628c7d2fb9
Jonathan <jonathan-ketelslegers@hotmail.com>
2023-03-15 20:25:58 +0100

    renamed stuff

--- app/src/main/java/com/example/movieApp/ui/theme/Theme.kt
+++ app/src/main/java/com/example/movieApp/ui/theme/Theme.kt
@@ -1,4 +1,4 @@
-package com.example.lab2.ui.theme
+package com.example.movieApp.ui.theme
 
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.MaterialTheme
--- app/src/test/java/com/example/movieApp/ExampleUnitTest.kt
+++ app/src/test/java/com/example/movieApp/ExampleUnitTest.kt
@@ -1,4 +1,4 @@
-package com.example.lab2
+package com.example.movieApp
 
 import org.junit.Test
 
