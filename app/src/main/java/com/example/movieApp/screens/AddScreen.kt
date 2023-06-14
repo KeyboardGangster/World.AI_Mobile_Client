c0b14c478a817ff7ef34d8e92e16a743b596350c
Nico Schulterer <91744414+Nico-Schulterer@users.noreply.github.com>
2023-06-14 14:23:06 +0200

    Updated AddScreen

--- app/src/main/java/com/example/movieApp/screens/AddScreen.kt
+++ app/src/main/java/com/example/movieApp/screens/AddScreen.kt
@@ -1,14 +1,32 @@
 package com.example.movieApp.screens
 
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.*
-import androidx.compose.runtime.*
+import androidx.compose.material.Button
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Bolt
+import androidx.compose.material.rememberScaffoldState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
@@ -41,7 +59,7 @@ fun AddScreen(navController: NavController) {
                 modifier = Modifier
                     .verticalScroll(rememberScrollState())
                     .fillMaxWidth(),
-                horizontalAlignment = Alignment.Start
+                horizontalAlignment = Alignment.CenterHorizontally
             ) {
 
                 var key by rememberSaveable {
@@ -74,15 +92,6 @@ fun AddScreen(navController: NavController) {
                 if (!cachedFilePaths.isNullOrEmpty())
                     PreviewImage(height = 200, path = cachedFilePaths[0])
 
-                Button(
-                    enabled = isEnabledGenerateButton,
-                    onClick = {
-                        /*Fetch world from server and update this ui*/
-                        viewModel.enqueueFetchRequest(prompt, key)
-                    }
-                ) {
-                    Text("Generate")
-                }
 
                 val validKey = TextInput(
                     modifier = Modifier,
@@ -94,17 +103,32 @@ fun AddScreen(navController: NavController) {
                     key = it
                 }
 
+                Text(text = "Crate a world with a description:",
+                    textAlign = TextAlign.Center,
+                    fontWeight = FontWeight.Normal
+                )
+
                 val validPrompt = TextInput(
                     modifier = Modifier.height(200.dp),
                     singleLine = false,
                     value = prompt,
-                    label = "Enter prompt.",
+                    label = "Enter prompt. Example: A rainy forest with huge trees...",
                     errorMsg = "Prompt must not be empty!",
                     predicate = { it.isNotEmpty() }) {
                     prompt = it
                 }
 
-                val validTags = SelectInput(modifier = Modifier,
+                Text(text = "Generate",
+                    style = MaterialTheme.typography.h6
+                    )
+                IconButton( enabled = isEnabledGenerateButton,
+                    onClick = {
+                        viewModel.enqueueFetchRequest(prompt, key)
+                    }) {
+                    Icon(Icons.Rounded.Bolt, "")
+                }
+
+                val validTags = SelectInput(modifier = Modifier.fillMaxWidth(),
                     genreItems = tagsItems,
                     label = "Select tags",
                     errorMsg = "Select at least one tag!",
