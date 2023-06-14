df2e169cfcf7245baf7d0df3b63a53ff96164dba
Nico Schulterer <91744414+Nico-Schulterer@users.noreply.github.com>
2023-06-14 10:10:11 +0200

    Updated DetailScreen

--- app/src/main/java/com/example/movieApp/screens/DetailScreen.kt
+++ app/src/main/java/com/example/movieApp/screens/DetailScreen.kt
@@ -1,16 +1,27 @@
 package com.example.movieApp.screens
 
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
 import androidx.compose.material.Text
-import androidx.compose.runtime.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.ArrowBack
+import androidx.compose.material.icons.rounded.Share
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.example.movieApp.viewmodel.DetailScreenViewModel
 import com.example.movieApp.widgets.DisplayTags
-import com.example.movieApp.widgets.WorldAllImages
 import com.example.movieApp.widgets.ScaffoldBottomBar
+import com.example.movieApp.widgets.WorldAllImages
 import kotlinx.coroutines.launch
 
 @Composable
@@ -19,7 +30,14 @@ fun DetailScreen(navController: NavController, viewModel: DetailScreenViewModel,
     val coroutineScope = rememberCoroutineScope()
 
     ScaffoldBottomBar(navController = navController) {
-        Column {
+
+        Column (modifier = Modifier.padding(10.dp)) {
+
+            IconButton(onClick = {
+            navController.navigateUp()
+        }) {
+            Icon(Icons.Rounded.ArrowBack, "")
+        }
             WorldAllImages(
                 height = 200,
                 world = world,
@@ -32,23 +50,19 @@ fun DetailScreen(navController: NavController, viewModel: DetailScreenViewModel,
             )
             Text(
                 text = "World Name",
-                fontSize = 30.sp
+                fontSize = 30.sp,
+                modifier = Modifier.padding(5.dp),
+                textAlign = TextAlign.Center
             )
+            IconButton(onClick = { /*TODO*/ }) {
+                    Icon(Icons.Rounded.Share, "Share this world")
+            }
             DisplayTags()
-            Text(text = "Some prompt that still needs to be stored in the DB, same with tags btw...")
-            Spacer(modifier = Modifier.fillMaxWidth().height(20.dp))
-            Text(text = "Seed: same as above. Isn't even implemented in World.AI yet...")
-            // SimpleAppBar(title = movie.title, navController = navController)
-            /*MovieRow(
-                movie = movie,
-                favForceUpdate = true,
-                onItemClick = {},
-                onFavClick = {
-                    coroutineScope.launch {
-                        viewModel.toggleFave(movie.id)
-                    }
-                })
-            ShowImages(urls = movie.images)*/
+            Text(text = "Some prompt that still needs to be stored in the DB, same with tags btw...", modifier = Modifier.padding(5.dp))
+            Spacer(modifier = Modifier
+                .fillMaxWidth()
+                .height(20.dp))
+            Text(text = "Seed: same as above. Isn't even implemented in World.AI yet...", modifier = Modifier.padding(5.dp))
         }
     }
 }
